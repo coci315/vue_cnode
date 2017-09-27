5a99f5e11b4450010cf0256e1e5ceb8f2b392e3a
coci315 <coci315@gmail.com>
2017-09-27 15:31:36 +0800

    change publicPath

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://coci315.tk/vue_node_dist/',
+    assetsPublicPath: 'http://vuecnode.linkw.tk/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
